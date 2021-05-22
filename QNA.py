ff10c4194bc3d7ea1dc28c78e917e1e27236123e
Leonardo de Farias <71597308+Tofudog@users.noreply.github.com>
2021-05-22 15:01:31 -0500

    Add files via upload
    Added QNA file for the computer to be able to read questions and answers directly from a file. It is preferable that this file is a text file.

---
+++ QNA.py
@@ -0,0 +1,12 @@
+# 1. What is the capital of France?
+# A. London
+# B. Paris
+# C. Croissont
+# D. Cheese
+# B
+# 2. Which of the following would a vegan NOT eat?
+# A. Lettuce
+# B. Lettuce
+# C. Lettuce
+# D. Ham, turkey, and buffalo sandwich
+# D 
