1632cebab94f1d207c152beae9be74accaa966d9
Leonardo de Farias <71597308+Tofudog@users.noreply.github.com>
2021-05-24 18:59:58 -0500

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -9,7 +9,7 @@ class QuizApplication:
     iteratively input questions with each answer choice (A-D)
     if consoleInputQNA is set to True. Otherwise, the questions are
     read from a file."""
-    def __init__(self, root=tk.Tk(), numQuestions=3, consoleInputQNA=True):
+    def __init__(self, root=tk.Tk(), numQuestions=3, consoleInputQNA=False):
         self.root = root
         self.numQuestions = numQuestions
         self.chosenLetter = None
@@ -23,21 +23,23 @@ class QuizApplication:
         self.labelQuestion = tk.Label(root, text="")
         self.buttonA = tk.Button(root, bg="red", text="")
         self.buttonB = tk.Button(root, bg="red", text="")
-        self.buttonC = tk.Button(root, bg="red", text="")
-        self.buttonD = tk.Button(root, bg="red", text="")
+        self.buttonC = tk.Button(root, bg="red", text="")                
+        self.buttonD = tk.Button(root, bg="red", text="")     
         self.next = tk.Button(root, bg="pink", text="next", command=self.nextButton)
-        self.back = tk.Button(root, bg="pink", text="back", command=self.backButton)
+        # self.back = tk.Button(root, bg="pink", text="back", command=self.backButton)
         self.confirmAnswer = tk.Button(root, bg="pink", text="select", command=self.confirm)
+        self.amountCorrectLabel = tk.Label(root, text=self.numCorrect)
         
         # Placing buttons and labels
         self.labelQuestion.grid(row=0, column=1, padx=10, pady=100)
-        self.buttonA.grid(row=1, column=1, padx=75, pady=25, ipadx=200, ipady=20)
-        self.buttonB.grid(row=2, column=1, padx=75, pady=25, ipadx=200, ipady=20)
-        self.buttonC.grid(row=3, column=1, padx=75, pady=25, ipadx=200, ipady=20)
-        self.buttonD.grid(row=4, column=1, padx=75, pady=25, ipadx=200, ipady=20)
+        self.buttonA.grid(row=1, column=1, padx=60, pady=25, ipadx=100, ipady=20, sticky="ew")
+        self.buttonB.grid(row=2, column=1, padx=60, pady=25, ipadx=100, ipady=20, sticky="ew")
+        self.buttonC.grid(row=3, column=1, padx=60, pady=25, ipadx=100, ipady=20, sticky="ew")
+        self.buttonD.grid(row=4, column=1, padx=60, pady=25, ipadx=100, ipady=20, sticky="ew")
         self.next.grid(row=5, column=2, padx=500, pady=0, ipadx=30, ipady=10)
-        self.back.grid(row=5, column=0, padx=10, pady=0, ipadx=30, ipady=10)
+        # self.back.grid(row=5, column=0, padx=10, pady=0, ipadx=30, ipady=10)
         self.confirmAnswer.grid(row=1, column=2, ipadx=30, ipady=10)
+        self.amountCorrectLabel.grid(row=0, column=2)
         
     # User-generated QNA if consoleInputQNA is True
     def inputQNA(self):
@@ -73,6 +75,11 @@ class QuizApplication:
         self.chosenLetter = letter
         
     def nextButton(self):
+        self.buttonA.config(state=tk.NORMAL)
+        self.buttonB.config(state=tk.NORMAL)
+        self.buttonC.config(state=tk.NORMAL)
+        self.buttonD.config(state=tk.NORMAL)
+        self.confirmAnswer.config(state=tk.NORMAL)
         self.currentQuestion += 1
         self.answerQuestion()
         
@@ -81,9 +88,15 @@ class QuizApplication:
         self.answerQuestion()
         
     def confirm(self):
-        if self.chosenLetter == self.answer1[-1]:
+        if self.chosenLetter == self.answer[-1]:
             self.numCorrect += 1
-            print("Correct")
+            self.amountCorrectLabel.config(text=self.numCorrect)
+            
+        self.buttonA.config(state=tk.DISABLED)
+        self.buttonB.config(state=tk.DISABLED)
+        self.buttonC.config(state=tk.DISABLED)
+        self.buttonD.config(state=tk.DISABLED)
+        self.confirmAnswer.config(state=tk.DISABLED)
     
     # Acutal quiz for user
     def answerQuestion(self): 
@@ -98,19 +111,9 @@ class QuizApplication:
         self.buttonD.config(text=self.answer[3], command=partial(self.answerUser, "D"))
     
 
-
-# quiz = QuizApplication(consoleInputQNA=False)
-# quiz.readQNAFromFile("QNA.py")
-# # tk.mainloop()
-# qna = quiz.QNA
-# q, a = qna.keys(), qna.values()
-# print(q)
-# print(a)
-
 newQuiz = QuizApplication(numQuestions=2)
-newQuiz.inputQNA()
-print(newQuiz.QNA)
-
+newQuiz.readQNAFromFile("QNA.py")
+tk.mainloop()
 
 
 
